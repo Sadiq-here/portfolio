858a0747f12abd7b9c3e2a4c7f547e183c5a5b03
Sadiq <mdsadique2912@gmail.com>
2024-03-06 20:34:57 +0530

    Home section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,13 @@ function linkAction() {
   navMenu.classList.remove('show-menu')
 }
 navLink.forEach((n) => n.addEventListener('click', linkAction))
+
+// ============SHADOW HEADER=================
+
+function scrollHeader() {
+  const nav = document.getElementById('header')
+  // When the scroll is greater than 200 viewport height, add the shadow-header class to the header tag
+  if (this.scrollY >= 80) nav.classList.add('shadow-header')
+  else nav.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', scrollHeader)
